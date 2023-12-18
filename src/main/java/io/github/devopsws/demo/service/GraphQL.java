7b9577d020cec566a71fa603dbdf2d489b5b63bf
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-12-18 16:57:31 +0800

    apply more graphql features (#13)
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- src/main/java/io/github/devopsws/demo/service/GraphQL.java
+++ src/main/java/io/github/devopsws/demo/service/GraphQL.java
@@ -2,6 +2,7 @@ package io.github.devopsws.demo.service;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import io.github.devopsws.demo.model.Book;
@@ -15,5 +16,15 @@ public class GraphQL {
     // @PreAuthorize("hasRole('USER')")
     public Book bookById(@Argument String id) {
         return Book.getById(id);
+    }
+
+    @QueryMapping
+    public Book[] books() {
+        return Book.allBooks();
+    }
+
+    @MutationMapping
+    public int addBook(@Argument String name, @Argument int pageCount) {
+        return Book.addBook(new Book("", name, pageCount, ""));
     }
 }
