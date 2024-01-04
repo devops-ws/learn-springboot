4f39910aabe5b1c8a80c376ef2b92e1415dd90cf
rick <LinuxSuRen@users.noreply.github.com>
2024-01-04 09:12:10 +0000

    fix: the trpc service is not ready

--- src/main/java/io/github/devopsws/demo/DemoApplication.java
+++ src/main/java/io/github/devopsws/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package io.github.devopsws.demo;
 
+import com.tencent.trpc.spring.boot.starters.annotation.EnableTRpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableTRpc
 @SpringBootApplication
 public class DemoApplication {
 
