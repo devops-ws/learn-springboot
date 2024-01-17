53afbefd62d2ca5e7b263deb6274e80503a505d6
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2024-01-17 14:01:40 +0800

    support to show openapi (#19)
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

---
+++ src/main/java/io/github/devopsws/demo/model/Message.java
@@ -0,0 +1,17 @@
+package io.github.devopsws.demo.model;
+
+public class Message {
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message=message;
+    }
+}
--- src/main/java/io/github/devopsws/demo/service/HealthService.java
+++ src/main/java/io/github/devopsws/demo/service/HealthService.java
@@ -2,11 +2,17 @@ package io.github.devopsws.demo.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.devopsws.demo.model.Message;
 
 @RestController
 public class HealthService {
 	@GetMapping("/health")
 	public String health() {
 		return "OK";
+	}
+	
+	@GetMapping("/health.json")
+	public Message healthJSON() {
+		return new Message("OK");
 	}
 }
