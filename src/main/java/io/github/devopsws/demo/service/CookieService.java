3ad1df5a185a70c6fc90e37004fb55a58db5490b
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2024-04-11 09:33:33 +0800

    return JSON string in the cookie api (#22)
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- src/main/java/io/github/devopsws/demo/service/CookieService.java
+++ src/main/java/io/github/devopsws/demo/service/CookieService.java
@@ -5,23 +5,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import io.github.devopsws.demo.model.Message;
 
 @RestController
 @RequestMapping("/cookies")
 public class CookieService {
     @GetMapping("/echo")
-    public String cookie(HttpServletRequest request, String name) {
-        Cookie[] cookies=        request.getCookies();
+    public Message<?> cookie(HttpServletRequest request, String name) {
+        Message<String> message = new Message<String>();
+        Cookie[] cookies = request.getCookies();
         if (cookies == null) {
-            return "cookies are empty";
-        }
+            message.setMessage("cookies are empty");
+        } else {
+            message.setMessage("not found");
 
-        for (int i = 0; i < cookies.length; i++) {
-            if (cookies[i].getName().equals(name)) {
-                return cookies[i].getValue();
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equals(name)) {
+                    message.setMessage(cookies[i].getValue());
+                    break;
+                }
             }
         }
 
-        return "not found";
+        return message;
     }   
 }
