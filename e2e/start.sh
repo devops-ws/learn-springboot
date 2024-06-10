0f8889e48772b4f568ca067777c2d85a4a03950f
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2024-06-10 18:43:01 +0800

    fix the e2e error with cookie (#27)
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- e2e/start.sh
+++ e2e/start.sh
@@ -7,27 +7,5 @@ then
 fi
 
 docker-compose version
-docker-compose -f "$file" up --build -d --pull=never
-
-while true
-do
-    docker-compose -f "$file" ps | grep testing
-    if [ $? -eq 1 ]
-    then
-        code=-1
-        docker-compose -f "$file" logs | grep server
-        docker-compose -f "$file" logs | grep testing
-        docker-compose ps -a | grep testing | grep "Exited (0)"
-        if [ $? -eq 0 ]
-        then
-            code=0
-            echo "successed"
-        fi
-        docker-compose ps -a
-
-        docker-compose -f "$file" down
-        set -e
-        exit $code
-    fi
-    sleep 1
-done
+docker-compose -f "$file" down
+docker-compose -f "$file" up --build testing --exit-code-from testing --remove-orphans
